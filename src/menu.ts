f8c42918184cb49fcc94fb4ff05fc2783e3eb1d2
maxjvjohansson <max.jv.johansson@gmail.com>
2025-09-05 10:54:22 +0200

    Add: Some menu styling with bg-image

--- src/menu.ts
+++ src/menu.ts
@@ -1,7 +1,8 @@
 import { init } from './main'
 
 const startBtn = document.getElementById('startBtn')!
+const menuRoot = document.getElementById('menu-root')!
 
 startBtn.addEventListener('click', () => {
-	init()
+	init(), (menuRoot.style.display = 'none')
 })
